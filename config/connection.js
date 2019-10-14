1b77048907ec41035831abecadc50dcebbc2c15f
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-10-13 19:09:29 -0500

    pruning files

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,6 @@
 const mysql = require("mysql");
 const credentials = require("../credentials");
 let connection;
-// const sequelize = require('sequelize-heroku').connect(require('sequelize'));
-
-// if (sequelize) {
-//     sequelize.authenticate().then( function() {
-//         var config = sequelize.connectionManager.config;
-//         console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
-        
-//         sequelize.query('SELECT 1+1 as test').then(function(res) {
-//             console.log('1+1='+res[0][0].test);
-//         });
-       
-//     }).catch( function(err) {
-//         var config = sequelize.connectionManager.config;
-//         console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
-//     });
-// } else {
-//     console.log('No environnement variable found.');
-// }
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
